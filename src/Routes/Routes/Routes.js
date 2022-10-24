692931effc20e744524977c607f08e840cabfe9d
Sehrish <sehrishzeba012@gmail.com>
2022-10-24 23:55:06 +0600

    2nd

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import SignUp from "../../Pages/Login/SignUp/SignUp";
+import Blogs from "../../Pages/Shared/Blogs/Blogs";
+import Courses from "../../Pages/Shared/Courses/Courses";
 
 export const routes = createBrowserRouter([
     {
@@ -10,6 +14,22 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/blogs',
+                element: <Blogs></Blogs>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signUp',
+                element: <SignUp></SignUp>
             }
         ]
     }
